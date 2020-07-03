0d5975822fec141bf6a6173735a5cba833f89e99
Aivaras <lukauskasaivaras@gmail.com>
2020-07-03 10:17:53 +0300

    Updates

---
+++ precache-manifest.cdf2a1bd0e6d625ce3c5d33751ce8dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8583b56679dc38ba34e8ecf624890d64",
+    "url": "/infinite-scroller/index.html"
+  },
+  {
+    "revision": "22a98bb51faf2b61a556",
+    "url": "/infinite-scroller/static/css/main.c9015053.chunk.css"
+  },
+  {
+    "revision": "a3cf7968be1b757395d7",
+    "url": "/infinite-scroller/static/js/2.91302fec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infinite-scroller/static/js/2.91302fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a98bb51faf2b61a556",
+    "url": "/infinite-scroller/static/js/main.349d125a.chunk.js"
+  },
+  {
+    "revision": "c5d543ce8f36048373ca",
+    "url": "/infinite-scroller/static/js/runtime-main.053d3eae.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/infinite-scroller/precache-manifest.cdf2a1bd0e6d625ce3c5d33751ce8dcc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/infinite-scroller/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
